1b651a165342bffc0f619657b8cacebf320185ea
Italo Almeida Aguiar <italo.almeida@outlook.com>
2019-12-19 12:54:37 -0300

    Inclusão do Swagger para documentação

--- TesteDTI/TesteDTI/Startup.cs
+++ TesteDTI/TesteDTI/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -11,6 +13,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using TesteDTI.Data;
 
 namespace TesteDTI
@@ -37,6 +40,15 @@ namespace TesteDTI
             services.AddHttpClient();
             services.AddServerSideBlazor();
             services.AddSingleton<GameService>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API jogo da velha DTI", Version = "v1" });
+                
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,6 +69,16 @@ namespace TesteDTI
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API jogo da velha DTI");
+            });
 
             app.UseEndpoints(endpoints =>
             {
